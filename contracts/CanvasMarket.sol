ab55ae0334cb894f3988638e87f5a2ec5ec410f1
kmisztal <krmisztal@gmail.com>
2018-03-11 16:43:55 +0800

    Added CanvasMarket

---
+++ contracts/CanvasMarket.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.21;
+
+import './BiddableCanvas.sol';
+
+/**
+* @dev This contract takes trading our artworks. Trading can happen
+*      if artwork has been initially bought. 
+*/
+contract CanvasMarket is BiddableCanvas { 
+
+}
