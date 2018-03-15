dc0d4c5699f939bad40907e25617bbb87fbd56f7
kmisztal <krmisztal@gmail.com>
2018-03-15 16:13:31 +0800

    Function to return canvas info

--- contracts/CryptoArt.sol
+++ contracts/CryptoArt.sol
@@ -7,4 +7,20 @@ import './CanvasMarket.sol';
 */
 contract CryptoArt is CanvasMarket { 
 
+    function getCanvasInfo(uint32 _artworkId) public view returns(CanvasInfo) {
+        Canvas storage canvas = _getCanvas(_artworkId);
+
+        return CanvasInfo(_artworkId, canvas.paintedPixelsCount, canvas.owner);
+    }
+
+    struct CanvasInfo {
+        uint32 id; 
+        uint32 paintedPixels;
+
+        /**
+        * @notice   Address can reffer to 0x0 if there is no owner. 
+        */
+        address owner; 
+    }
+
 } 
