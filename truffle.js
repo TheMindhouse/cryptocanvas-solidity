917acef87a491ffa0e10bc776d2154d1259d3bb9
kmisztal <krmisztal@gmail.com>
2018-04-05 20:08:26 +0800

    Configured tests

--- truffle.js
+++ truffle.js
@@ -1,4 +1,18 @@
+require('babel-register');
+require('babel-polyfill');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        // development: {
+        //     host: "localhost",
+        //     port: 8545,
+        //     network_id: "*", // Match any network id
+        //     gas: 6500000
+        // },
+    },
+    mocha: {
+        useColors: true
+    }
 };
