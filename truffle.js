15bb0445a5d1cbc0bdbc096cee750f69f83be3ca
kmisztal <krmisztal@gmail.com>
2018-06-14 11:48:01 +0200

    Configured test network for local migrations

--- truffle.js
+++ truffle.js
@@ -4,14 +4,14 @@ require('babel-polyfill');
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
-    // networks: {
-    //     test: {
-    //         host: "localhost",
-    //         port: 8545,
-    //         network_id: "*", // Match any network id
-    //         gas: 6500000
-    //     },
-    // },
+    networks: {
+        test: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*", // Match any network id
+            gas: 7000000
+        },
+    },
     mocha: {
         useColors: true
     }
